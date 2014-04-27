c11da13cf62faa61e0dd06012a3a96e485afa4dd
raw99 <raw9965@gmail.com>
2014-04-27 18:40:07 +0300

    let's start from zero...

---
+++ common/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Sun Apr 27 18:35:16 EEST 2014"
+#define LINUX_COMPILE_TIME "18:35:16"
+#define LINUX_COMPILE_BY "rw"
+#define LINUX_COMPILE_HOST "zemene-PC"
+#define LINUX_COMPILE_DOMAIN "workgroup"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
