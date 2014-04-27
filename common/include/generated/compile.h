10ae9a7d65d37991ce3076295f8d8433db066e30
raw99 <raw9965@gmail.com>
2014-04-28 00:27:47 +0300

    some stuff

--- common/include/generated/compile.h
+++ common/include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 9 */
+/* This file is auto generated, version 12 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#9 PREEMPT Sun Apr 27 21:37:37 EEST 2014"
-#define LINUX_COMPILE_TIME "21:37:37"
+#define UTS_VERSION "#12 PREEMPT Mon Apr 28 00:13:30 EEST 2014"
+#define LINUX_COMPILE_TIME "00:13:30"
 #define LINUX_COMPILE_BY "rw"
 #define LINUX_COMPILE_HOST "zemene-PC"
 #define LINUX_COMPILE_DOMAIN "workgroup"
