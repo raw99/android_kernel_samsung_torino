2809619b48850660949aafa2d1b5841e8a3fcb2c
raw99 <raw9965@gmail.com>
2014-05-31 12:01:15 +0300

    ...

--- common/include/generated/compile.h
+++ common/include/generated/compile.h
@@ -1,9 +1,9 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 3 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 PREEMPT Tue Apr 29 19:37:52 EEST 2014"
-#define LINUX_COMPILE_TIME "19:37:52"
+#define UTS_VERSION "#3 PREEMPT Sat May 31 12:00:20 EEST 2014"
+#define LINUX_COMPILE_TIME "12:00:20"
 #define LINUX_COMPILE_BY "rw"
-#define LINUX_COMPILE_HOST "zemene-PC"
+#define LINUX_COMPILE_HOST "test-PC"
 #define LINUX_COMPILE_DOMAIN "workgroup"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
